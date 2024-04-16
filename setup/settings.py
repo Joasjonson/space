70a0622c0b83d14674e623f0e91911232a0783b5
Joás Jonson <joasjonson@gmail.com>
2024-04-16 17:43:01 +0200

    add bd

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'space',
+    'space.apps.SpaceConfig',
 ]
 
 MIDDLEWARE = [
--- space/views.py
+++ space/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'space/index.html')
+
+    dados = {1:{'nome': 'Galaxia X014',
+                'legenda': 'NASA / WEB / JAMES WEBB '},
+             2:{'nome': 'Galaxia G546', 
+                'legenda': 'NASA / HUUB / WEB'}   
+             }
+
+    return render(request, 'space/index.html', {'cards': dados} )
 
 def imagem(request):
     return render(request, 'space/imagem.html')
