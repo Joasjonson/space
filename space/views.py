99f2136a04e4b838e7e33f3e0433fe5983fb0637
Joás Jonson <114940204+Joasjonson@users.noreply.github.com>
2024-05-24 19:47:57 +0100

    add auth user

--- space/views.py
+++ space/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from space.models import Fotografia
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect("login")
+    
     fotografia = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     return render(request, 'space/index.html', {'cards': fotografia} )
 
@@ -13,6 +18,10 @@ def imagem(request, foto_id):
 
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect("login")
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
@@ -21,3 +30,4 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_buscar)           # Retorna dados de acordo com a palavra digitada no campo de busca
 
     return render(request, "buscar.html", {"cards": fotografias})
+
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForms()
+
+    if request.method == "POST":
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
+
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha,
+        )
+
+        if usuario is not None:
+            auth.login(request, usuario)
+            messages.success(request, f"{nome} logado com sucesso !")
+            return redirect("index")
+        
+        else:
+            messages.error(request, "Algo deu errado, verifique suas informaçoes .")
+            return redirect("login")
+
     return render(request, "usuarios/login.html", {"form": form})
+
 
 def cadastro(request):
     form = CadastroForms()
+
+    if request.method == "POST":
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            if form['senha'].value() != form['confirma_senha'].value():
+                messages.error(request, "Senhas nao conferem !")
+                return redirect("cadastro")
+            
+            nome = form["nome_usuario"].value()
+            email = form["email"].value()
+            senha = form["senha"].value()
+
+            if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuario ja existente !")
+                return redirect("cadastro")
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            messages.success(request, f"Cadastro efetuado com sucesso ! Faça Login ")
+            return redirect("login")
+            
     return render(request, "usuarios/cadastro.html", {"form": form})
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout efetuado !")
+    return redirect('login')
